04b14cc017d9435cd43039ec9193a5021800a4ec
Alexis BIZON <alexis.bizon@biz-cho.ovh>
2020-01-23 10:48:57 +0100

    changement des droits sur les fichiers de sortie

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,3 +15,7 @@ cd /latex
 echo "Compilation du latex en pdf"
 
 pdflatex -shell-escape -output-directory=/latex/user_content/output -jobname=${OUTPUT_PDF_FILENAME} ${ENTRYPOINT_LATEX_FILE}
+
+echo "Changement des droits sur les fichiers de sortie"
+
+chmod -R 666 /laext/user_content
